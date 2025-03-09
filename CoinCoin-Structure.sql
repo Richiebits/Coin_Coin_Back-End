57b73ed7dc7de58ea18b92c8b5175ba239d40ee7
nikolasunjka <nikolasunjka23@gmail.com>
2025-03-09 16:36:31 -0400

    correction erreur

--- CoinCoin-Structure.sql
+++ CoinCoin-Structure.sql
@@ -12,12 +12,12 @@ DROP TABLE Client CASCADE CONSTRAINTS;
 ---- Création de la table Client
 ----------------------------------------------
 CREATE TABLE Client (
-    id            INTEGER(10)    NOT NULL,
-    nom           VARCHAR(255)   NOT NULL,
-    prenom        VARCHAR(255)   NOT NULL,
-    tel           VARCHAR(255)   NULL,
-    email         VARCHAR(255)   NOT NULL,
-    mot_de_passe  VARCHAR(255)   NOT NULL,
+    id            NUMBER(10)    NOT NULL,
+    nom           VARCHAR2(255)   NOT NULL,
+    prenom        VARCHAR2(255)   NOT NULL,
+    tel           VARCHAR2(255)   NULL,
+    email         VARCHAR2(255)   NOT NULL,
+    mot_de_passe  VARCHAR2(255)   NOT NULL,
     
     CONSTRAINT PK_Client PRIMARY KEY (id)
 );
@@ -26,10 +26,10 @@ CREATE TABLE Client (
 ---- Création de la table Projet
 ----------------------------------------------
 CREATE TABLE Projet (
-    id          INTEGER(10)    NOT NULL,
-    nom         VARCHAR(255)   NOT NULL,
-    but_epargne INTEGER(10)    NOT NULL,
-    Clientid    INTEGER(10)    NOT NULL,
+    id          NUMBER(10)    NOT NULL,
+    nom         VARCHAR2(255)   NOT NULL,
+    but_epargne NUMBER(10)    NOT NULL,
+    Clientid    NUMBER(10)    NOT NULL,
     
     CONSTRAINT PK_Projet PRIMARY KEY (id),
     CONSTRAINT FK_Projet_Client FOREIGN KEY (Clientid) REFERENCES Client(id) ON DELETE CASCADE
@@ -39,13 +39,13 @@ CREATE TABLE Projet (
 ---- Création de la table Budget
 ----------------------------------------------
 CREATE TABLE Budget (
-    id              INTEGER(10)    NOT NULL,
-    nom             VARCHAR(255)   NOT NULL,
-    depenses_total  INTEGER(10)    NOT NULL,
-    revenus_total   INTEGER(10)    NOT NULL,
+    id              NUMBER(10)    NOT NULL,
+    nom             VARCHAR2(255)   NOT NULL,
+    depenses_total  NUMBER(10)    NOT NULL,
+    revenus_total   NUMBER(10)    NOT NULL,
     date_debut      DATE           NOT NULL,
     date_fin        DATE           NOT NULL,
-    Projetid        INTEGER(10)    NOT NULL,
+    Projetid        NUMBER(10)    NOT NULL,
     
     CONSTRAINT PK_Budget PRIMARY KEY (id),
     CONSTRAINT FK_Budget_Projet FOREIGN KEY (Projetid) REFERENCES Projet(id) ON DELETE CASCADE
@@ -55,10 +55,10 @@ CREATE TABLE Budget (
 ---- Création de la table Revenu
 ----------------------------------------------
 CREATE TABLE Revenu (
-    id_revenu  INTEGER(10)   NOT NULL,
-    nom        VARCHAR(255)  NOT NULL,
-    montant    DOUBLE(10)    NOT NULL,
-    Budgetid   INTEGER(10)   NOT NULL,
+    id_revenu  NUMBER(10)   NOT NULL,
+    nom        VARCHAR2(255)  NOT NULL,
+    montant    NUMBER(10)    NOT NULL,
+    Budgetid   NUMBER(10)   NOT NULL,
     
     CONSTRAINT PK_Revenu PRIMARY KEY (id_revenu),
     CONSTRAINT FK_Revenu_Budget FOREIGN KEY (Budgetid) REFERENCES Budget(id) ON DELETE CASCADE
@@ -68,10 +68,10 @@ CREATE TABLE Revenu (
 ---- Création de la table Depense
 ----------------------------------------------
 CREATE TABLE Depense (
-    id_depense  INTEGER(10)   NOT NULL,
-    nom         VARCHAR(255)  NOT NULL,
-    montant     DOUBLE(10)    NOT NULL,
-    Budgetid    INTEGER(10)   NOT NULL,
+    id_depense  NUMBER(10)   NOT NULL,
+    nom         VARCHAR2(255)  NOT NULL,
+    montant     NUMBER(10)    NOT NULL,
+    Budgetid    NUMBER(10)   NOT NULL,
     
     CONSTRAINT PK_Depense PRIMARY KEY (id_depense),
     CONSTRAINT FK_Depense_Budget FOREIGN KEY (Budgetid) REFERENCES Budget(id) ON DELETE CASCADE
@@ -81,11 +81,11 @@ CREATE TABLE Depense (
 ---- Création de la table Historique
 ----------------------------------------------
 CREATE TABLE Historique (
-    id         INTEGER(10)   NOT NULL,
-    Projetid   INTEGER(10)   NOT NULL,
-    date       DATE          NOT NULL,
-    type       VARCHAR(255)  NOT NULL,
-    montant    INTEGER(10)   NULL,
+    id         NUMBER(10)   NOT NULL,
+    Projetid   NUMBER(10)   NOT NULL,
+    date_histo DATE          NOT NULL,
+    type       VARCHAR2(255)  NOT NULL,
+    montant    NUMBER(10)   NULL,
     
     CONSTRAINT PK_Historique PRIMARY KEY (id),
     CONSTRAINT FK_Historique_Projet FOREIGN KEY (Projetid) REFERENCES Projet(id) ON DELETE CASCADE
